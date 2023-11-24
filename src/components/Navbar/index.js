6c8099aaca9314872e758f100a9a2c449b5ec194
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-11-24 12:27:11 -0300

    Boosting the navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,40 @@
+import styles from './Navbar.module.scss';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import classNames from 'classnames';
+import {
+  RiShoppingCart2Line,
+  RiShoppingCartFill
+} from 'react-icons/ri';
+
+const iconProps = {
+  color: 'white',
+  size: 24
+}
+
+export default function Navbar() {
+  return (
+    <nav className={styles.nav}>
+      <Logo className={styles.logo} />
+      <div className={styles.link}>
+        <div>
+          <a href='/' className={classNames(styles.link, {
+            [styles.selected]: window.location.pathname === '/'
+          })}>
+            Home
+          </a>
+        </div>
+      </div>
+      <div className={styles.search}>
+
+      </div>
+      <div className={styles.icons}>
+        <a href='/carrinho'>
+          {window.location.pathname === 'carrinho'
+            ? <RiShoppingCartFill {...iconProps} />
+            : <RiShoppingCart2Line {...iconProps} />
+          }
+        </a>
+      </div>
+    </nav>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>Hello World!</div>
+    <Navbar />
   </React.StrictMode>
 );
