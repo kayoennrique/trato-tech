b728509805416294ec2667abfaad6027e514c548
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-11-26 15:30:55 -0300

    Creating advertise route

---
+++ src/pages/Advertise/index.js
@@ -0,0 +1,7 @@
+export default function Advertise() {
+    return(
+        <div>
+            Anuncie
+        </div>
+    );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import PageDefault from "components/PageDefault";
+import Advertise from "pages/Advertise";
 import Cart from "pages/Cart";
 import Categorie from "pages/Categorie";
 import Home from "pages/Home";
@@ -8,13 +9,11 @@ export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route
-                    path='/'
-                    element={<PageDefault />}
-                >
+                <Route path='/' element={<PageDefault />} >
                     <Route index element={<Home />} />
                     <Route path='/categoria/:nameCategorie' element={<Categorie />} />
                     <Route path='/carrinho' element={<Cart />} />
+                    <Route path='/anuncie' element={<Advertise />} />
                 </Route>
             </Routes>
         </BrowserRouter >
