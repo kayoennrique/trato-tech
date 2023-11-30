51732f5ec2dfbd058ef92df3d060d9679795cfc1
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-11-30 16:15:36 -0300

    Controlling payment

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -2,37 +2,51 @@ import Header from 'components/Header';
 import styles from './Payment.module.scss';
 import Select from 'components/Select';
 import Button from 'components/Button';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadPayment } from 'store/reducers/cart';
 
 export default function Payment() {
+  const [formOfPayment, setFormOfPayment] = useState('-');
   const dispatch = useDispatch();
   const user = useSelector(state => state.user);
   const total = useSelector(state => state.cart.total);
+  const valueTotal = formOfPayment === '-' ? total : total * formOfPayment.rate;
+
+  function changePaymentMethod(e) {
+    if (e.target.value === '-') return setFormOfPayment('-');
+
+    setFormOfPayment(user.cards.find(card => card.id === e.target.value));
+  }
 
   useEffect(() => {
     dispatch(loadPayment());
-  }, [dispatch])
+  }, [dispatch]);
 
   return (
     <div className={styles.container}>
       <Header title='Pagamento' />
       <div className={styles.data}>
         <p className={styles.form}> Olá {user.name}! Escolha a forma de pagamento: </p>
-        <Select placeholder='Forma de pagamento' alt='Forma de pagamento'>
+        <Select value={formOfPayment.id} onChange={changePaymentMethod} placeholder='Forma de pagamento' alt='Forma de pagamento'>
           <option value='-'> Forma de pagamento </option>
           {user.cards?.map(card => (
             <option key={card.id} value={card.id}>{card.name}</option>
           ))}
         </Select>
         <div className={styles.content}>
-          <p> Total com taxas: R$ 0.00 {total.toFixed(2)}</p>
+          {formOfPayment !== '-' && (
+            <>
+              <p>A forma de pagamento {formOfPayment.name} tem taxa {formOfPayment.rate} x</p>
+              <p>O saldo deste cartão é de R$ {formOfPayment.balance.toFixed(2)}</p>
+            </>
+          )}
+          <p> Total com taxas: R$ {valueTotal.toFixed(2)}</p>
         </div>
         <div className={styles.finish}>
           <Button> Finalizar Compra </Button>
         </div>
       </div>
     </div>
-  )
+  );
 }
